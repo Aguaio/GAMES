5fc2740f7e04e42785d645e71fed3b47f538e1f0
Aguaio <8towea@gmail.com>
2026-02-15 16:02:08 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'django-insecure-lr*l3vj*u4=#qv6i*om=0hzqg-*()c8d^%%mh2sfkrek(8-z&j
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -102,3 +102,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
