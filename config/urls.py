d3a59a22ba491e4c7507b6217b3376edf2ade006
Aguaio <8towea@gmail.com>
2026-02-15 15:43:35 -0300

    Add files via upload

---
+++ config/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from juego import views
+
+urlpatterns = [
+    # --- 1. RUTAS DE ADMINISTRACIÓN ---
+    # (Las mantenemos aquí o podríamos moverlas a juego/urls.py después)
+    path('sistema/login/', views.login_admin_custom, name='login_admin'),
+    path('sistema/panel/', views.panel_control, name='panel_control'),
+    path('sistema/logout/', views.logout_admin, name='logout_admin'),
+
+    # --- 2. INCLUIR LAS RUTAS DE LA APP 'JUEGO' ---
+    # Esto conecta con juego/urls.py, donde están 'inicio', 'sala_espera'
+    # Y LO MÁS IMPORTANTE: las nuevas APIs que te estaban fallando.
+    path('', include('juego.urls')),
+]
