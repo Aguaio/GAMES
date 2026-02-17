bc2f4ed15077b6919005b76c42de3508eb7d7d93
Aguaio <8towea@gmail.com>
2026-02-17 02:38:32 -0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from juego import views
 urlpatterns = [
     # --- 1. RUTAS DE ADMINISTRACIÓN ---
     # (Las mantenemos aquí o podríamos moverlas a juego/urls.py después)
-    path('sistema/login/', views.login_admin_custom, name='login_admin'),
+    path('sistema/login/', views.login_admin, name='login_admin'),
     path('sistema/panel/', views.panel_control, name='panel_control'),
     path('sistema/logout/', views.logout_admin, name='logout_admin'),
 
@@ -13,4 +13,5 @@ urlpatterns = [
     # Esto conecta con juego/urls.py, donde están 'inicio', 'sala_espera'
     # Y LO MÁS IMPORTANTE: las nuevas APIs que te estaban fallando.
     path('', include('juego.urls')),
+
 ]
